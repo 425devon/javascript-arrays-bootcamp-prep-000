3f7ba86727f9f2156b1ec2ad78efe206d0fddf39
Devon Langendoerfer <425devon@gmail.com>
2017-11-09 04:51:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray(arr, n){
+  var newArr = [];
+  newArr = [n, ...arr]
+  return newArr;
+}
